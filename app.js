0378dd7dbfc2fbf33eff44de4be468b54de58136
ELisanna <elisannamartinezsanchez@gmail.com>
2025-08-25 16:43:34 -0400

    Agrega comentarios para mayor entendimiento

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
+// Declarar arreglo para almacenar los nombres
 let amigos = [];
 
 function agregarAmigo() {
+    // Obtener valor del input y almacenarlo en una variable
     let nombre = document.getElementById('amigo');
+    // Si el input esta en blanco o solo con espacios manda un mensaje
     if (nombre.value.trim() === "") {
         alert("Por favor, inserte un nombre.")
     }
+    // Si no: Guarda el nombre en el arreglo y limpia el input
     else{
         amigos.push(nombre.value);
         nombre.value = "";
