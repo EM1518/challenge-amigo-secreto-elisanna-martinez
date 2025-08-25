2bd407da059aec436269ea40b6516d36068d7416
ELisanna <elisannamartinezsanchez@gmail.com>
2025-08-25 16:39:24 -0400

    Implementa función para actualizar la lista de amigos

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+    let nombre = document.getElementById('amigo');
+    if (nombre.value.trim() === "") {
+        alert("Por favor, inserte un nombre.")
+    }
+    else{
+        amigos.push(nombre.value);
+        nombre.value = "";
+        console.log(amigos);
+    }
+
+};
+
